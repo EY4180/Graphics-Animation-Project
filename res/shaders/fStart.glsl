6a33404e53402211f9063f28deb4623b94896668
Edward Yamamoto <kfcs700@gmail.com>
2021-04-19 21:11:12 +0800

    testing changes to vertex shader

--- res/shaders/fStart.glsl
+++ res/shaders/fStart.glsl
@@ -1,56 +1,40 @@
 varying vec4 color;
 varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+varying vec3 Intensity; // attenuation of light
+varying vec3 Light; // vector from point to light
+varying vec3 Eye; // vector from point to eye
+varying vec3 Normal; // surface normal vector
+varying vec3 Half; // vector from point to eye
 
 uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform float texScale;
 uniform sampler2D texture;
 uniform float Shininess;
 
-in vec3 Lvec; // vector from point to light
-in vec3 Evec; // vector from point to eye
-in vec3 Nvec; // surface normal vector
-
-// calculate lighting based on linear, quadratic and constant attenuation
-void vertexLighting(out float attenuation, in float lightDistance)
-{
-    float csrc = 100.0; // source intensity 
-    float kc = 5.0; // constant attenuation
-    float kl = 1.0; // linear attenuation
-    float kq = 2.0; // quadratic attenuation
-    attenuation = csrc / (kc + kl * lightDistance + kq * pow(lightDistance, 2.0));
-}
-
 void main()
 {
-    vec3 Normal = normalize(Nvec);
-    vec3 Light = normalize(Lvec);
-    vec3 Eye = normalize(Evec);
-    vec3 Half = normalize( Light + Eye );  // Halfway vector
-    float multiplier = 1.0;
-    vertexLighting(multiplier, length(Lvec));
-
-    vec3 ambient = AmbientProduct * multiplier;
+    vec3 ambient = AmbientProduct;
 
     float Kd = max( dot(Light, Normal), 0.0 );
-    vec3 diffuse = Kd*DiffuseProduct * multiplier;
+    vec3 diffuse = Kd*DiffuseProduct;
 
     float m = 0.5;
     // pretty simple method for gettin a better specular coefficient
     // Gaussian Distribution m is a user defined constant for smoothness between 0 and 1
     float KsNew = exp(pow((dot(Normal, Half) / m), 2.0));
     float Ks = pow( max(dot(Normal, Half), 0.0), Shininess );
-    vec3 specular = KsNew * SpecularProduct  * multiplier;    
+    vec3 specular = KsNew * SpecularProduct;    
     if (dot(Light, Normal) < 0.0 ) {
 	    specular = vec3(0.0, 0.0, 0.0);
     } 
 
-    vec4 perFragment = vec4( ambient.rgb + diffuse.rgb + specular.rgb, 1.0 );
+    vec4 perFragment = vec4( (ambient.rgb + diffuse.rgb + specular.rgb) * Intensity, 1.0 );
 
     if(gl_FrontFacing) {
         gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
     }
     else {
-        gl_FragColor = perFragment * texture2D( texture, texCoord * texScale );
+        gl_FragColor = color * texture2D( texture, texCoord * texScale );
     }
     
 }
--- res/shaders/vStart.glsl
+++ res/shaders/vStart.glsl
@@ -4,6 +4,11 @@ attribute vec2 vTexCoord;
 
 varying vec2 texCoord;
 varying vec4 color;
+varying vec3 Intensity; // attenuation of light
+varying vec3 Light; // vector from point to light
+varying vec3 Eye; // vector from point to eye
+varying vec3 Normal; // surface normal vector
+varying vec3 Half; // vector from point to eye
 
 uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform vec3 GlobalAmbient;
@@ -12,62 +17,54 @@ uniform mat4 Projection;
 uniform vec4 LightPosition;
 uniform float Shininess;
 
-varying vec3 Lvec; // vector from point to light
-varying vec3 Evec; // vector from point to eye
-varying vec3 Nvec; // surface normal vector
 
 // calculate lighting based on linear, quadratic and constant attenuation
-void vertexLighting(out float attenuation, in float lightDistance)
+void getAttenuation(out vec3 attenuation, in vec3 light)
 {
     float csrc = 100.0; // source intensity 
     float kc = 5.0; // constant attenuation
     float kl = 0.5; // linear attenuation
     float kq = 0.5; // quadratic attenuation
-    attenuation = csrc / (kc + kl * lightDistance + kq * pow(lightDistance, 2.0));
+    float total = csrc / (kc + kl * length(light) + kq * pow(length(light), 2.0));
+    attenuation = vec3(total, total, total);
 }
 
 void main()
 {
     vec4 vpos = vec4(vPosition, 1.0);
-
     // Transform vertex position into eye coordinates
     vec3 pos = (ModelView * vpos).xyz;
-
     // The vector to the light from the vertex    
-    Lvec = LightPosition.xyz - pos;
-    
-    float Cl = 1.0;
-    vertexLighting(Cl, length(Lvec));
+    vec3 Lvec = LightPosition.xyz - pos;
+
 
     // Unit direction vectors for Blinn-Phong shading calculation
-    vec3 L = normalize( Lvec );   // Direction to the light source
-    vec3 E = normalize( -pos );   // Direction to the eye/camera
-    vec3 H = normalize( L + E );  // Halfway vector
+    Light = normalize( Lvec );   // Direction to the light source
+    Eye = normalize( -pos );   // Direction to the eye/camera
+    Half = normalize( Light + Eye );  // Halfway vector
+    getAttenuation(Intensity, Lvec); // Light intensity
 
     // Transform vertex normal into eye coordinates (assumes scaling
     // is uniform across dimensions)
-    vec3 N = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
+    Normal = normalize( (ModelView*vec4(vNormal, 0.0)).xyz );
     
     // Compute terms in the illumination equation
-    vec3 ambient = AmbientProduct * Cl;
+    vec3 ambient = AmbientProduct;
 
-    float Kd = max( dot(L, N), 0.0 );
-    vec3  diffuse = Kd*DiffuseProduct * Cl;
+    float Kd = max( dot(Light, Normal), 0.0 );
+    vec3  diffuse = Kd*DiffuseProduct;
 
-    float Ks = pow( max(dot(N, H), 0.0), Shininess );
-    vec3  specular = Ks * SpecularProduct * Cl;
+    float Ks = pow( max(dot(Normal, Half), 0.0), Shininess );
+    vec3  specular = Ks * SpecularProduct;
     
-    if (dot(L, N) < 0.0 ) {
-	specular = vec3(0.0, 0.0, 0.0);
+    if (dot(Light, Normal) < 0.0 ) {
+	    specular = vec3(0.0, 0.0, 0.0);
     } 
 
     // globalAmbient is independent of distance from the light source
-    color.rgb = GlobalAmbient + ambient + diffuse + specular;
+    color.rgb = GlobalAmbient + (ambient + diffuse + specular) * Intensity;
     color.a = 1.0;
 
     gl_Position = Projection * ModelView * vpos;
-
     texCoord = vTexCoord;
-    Nvec = N;
-    Evec = -pos;
 }
